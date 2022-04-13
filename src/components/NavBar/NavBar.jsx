63d2653cdba95557b48424de6347dcd1f5546380
Carolina Pirela <caropi30@gmail.com>
2022-04-13 13:25:17 -0400

    navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,25 +11,26 @@ import { FaShoppingCart } from 'react-icons/fa';
 function NavBar(){
     return(
         <Navbar bg="light" expand="lg">
-        <Container fluid classNAme="d-flex justify-content-center">
-            <Navbar.Brand href="#" >
-                <figure>
-                    <img src={image} alt="Entel Logo"/>
-                </figure>
-                <h1>Tienda Entel</h1>
+        <Container fluid classNAme="d-flex justify-content-between">
+            <Navbar.Brand href="#" className="d-flex">
+                <div className="brand d-flex align-items-center">
+                    <figure>
+                        <img src={image} alt="Entel Logo"/>
+                    </figure>
+                </div>
+                
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-            <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
-                navbarScroll
-            >
-                <Nav.Link href="#">Home</Nav.Link>
-                <Nav.Link href="#">Equipos</Nav.Link>
-                <Nav.Link href="#">Accesorios</Nav.Link>
-            </Nav>
-            <Button variant="primary"><FaShoppingCart /></Button>{' '}
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        navbarScroll
+                    >
+                        <Nav.Link href="#">Home</Nav.Link>
+                        <Nav.Link href="#">Equipos</Nav.Link>
+                        <Nav.Link href="#">Accesorios</Nav.Link>
+                    </Nav>
+                <Button variant="primary" className="me-5">Comprar <FaShoppingCart /></Button>{' '}
             </Navbar.Collapse>
         </Container>
     </Navbar> 
