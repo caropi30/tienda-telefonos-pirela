9713e567db76f0e745bcdd395801b13e2034c68d
Carolina Pirela <caropi30@gmail.com>
2022-04-13 12:20:50 -0400

    nombre tienda

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -16,6 +16,7 @@ function NavBar(){
                 <figure>
                     <img src={image} alt="Entel Logo"/>
                 </figure>
+                <h1>Tienda Entel</h1>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
