2c8bad637dba61a882b151b29d13f8b3b5dba8e0
Carolina Pirela <caropi30@gmail.com>
2022-05-12 18:04:51 -0400

    context

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -2,5 +2,10 @@ import { createContext, useContext } from "react";
 
 export const CartContext = createContext({});
 
-
 export const useCartContext = () => useContext(CartContext);
+
+const CartContextProvider = ({ children }) => {
+  return <CartContext.Provider>{children}</CartContext.Provider>;
+};
+
+export default CartContextProvider;
